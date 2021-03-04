46a275bdae1a75c42e2ce934fe9c8de0688971c0
gabrieltherock <gabrieldarocha2013@hotmail.com>
2021-03-04 20:28:18 -0300

    mostrando os detalhes de cada post

--- src/main/java/br/com/gabriel/blog/controller/BlogController.java
+++ src/main/java/br/com/gabriel/blog/controller/BlogController.java
@@ -5,6 +5,7 @@ import br.com.gabriel.blog.service.BlogService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.servlet.ModelAndView;
 
 import java.util.List;
@@ -20,6 +21,14 @@ public class BlogController {
         ModelAndView mv = new ModelAndView("posts");
         List<Post> posts = blogService.findAll();
         mv.addObject("posts", posts);
+        return mv;
+    }
+
+    @GetMapping(value="/posts/{id}")
+    public ModelAndView getPostDetails(@PathVariable("id") Long id) {
+        ModelAndView mv = new ModelAndView("postDetails");
+        Post post = blogService.findById(id);
+        mv.addObject("post", post);
         return mv;
     }
 }
