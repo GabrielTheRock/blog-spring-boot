d692e711ffce35bc890c3d25989c7742bf1c86e4
gabrieltherock <gabrieldarocha2013@hotmail.com>
2021-03-05 01:00:51 -0300

    formulario para adicionar novos posts.html

--- src/main/java/br/com/gabriel/blog/controller/BlogController.java
+++ src/main/java/br/com/gabriel/blog/controller/BlogController.java
@@ -4,10 +4,15 @@ import br.com.gabriel.blog.model.Post;
 import br.com.gabriel.blog.service.BlogService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
+import javax.validation.Valid;
+import java.time.LocalDate;
 import java.util.List;
 
 @Controller
@@ -30,5 +35,20 @@ public class BlogController {
         Post post = blogService.findById(id);
         mv.addObject("post", post);
         return mv;
+    }
+
+    @GetMapping(value="/newpost")
+    public String getPostForm() {
+        return "postForm";
+    }
+
+    @PostMapping(value="/newpost")
+    public String savePost(@Valid Post post, BindingResult result, RedirectAttributes attributes) {
+        if (result.hasErrors()) {
+            return "redirect:/newpost";
+        }
+        post.setData(LocalDate.now());
+        blogService.save(post);
+        return "redirect:/posts";
     }
 }
